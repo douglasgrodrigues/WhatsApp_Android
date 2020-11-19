79e3fe928cc032472a7fb2cbebd2dd48928775c5
Douglas Ghiraldello Rodrigues <d.roodrigues-rs@hotmail.com>
2020-11-19 18:26:18 -0300

    Atualização após implementar as listagens de conversas e contatos.

--- app/src/main/java/com/example/whatsapp/activity/fragment/ConversasFragment.java
+++ app/src/main/java/com/example/whatsapp/activity/fragment/ConversasFragment.java
@@ -3,22 +3,47 @@ package com.example.whatsapp.activity.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.whatsapp.R;
+import com.example.whatsapp.activity.adapter.ConversasAdapter;
+import com.example.whatsapp.activity.config.ConfiguracaoFirebase;
+import com.example.whatsapp.activity.helper.UsuarioFirebase;
+import com.example.whatsapp.activity.model.Conversa;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ConversasFragment extends Fragment {
 
+    private RecyclerView recyclerViewConversas;
+    private List<Conversa> listaConversas = new ArrayList<>();
+    private ConversasAdapter adapter;
+    private DatabaseReference database;
+    private DatabaseReference conversasRef;
+    private ChildEventListener childEventListenerConversas;
+
+
+
 
     public ConversasFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -26,7 +51,79 @@ public class ConversasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conversas, container, false);
+        View view = inflater.inflate(R.layout.fragment_conversas, container, false);
+
+        recyclerViewConversas = view.findViewById(R.id.recyclerListaConversas);
+
+        //Congirurar o adapter
+        adapter = new ConversasAdapter(listaConversas, getActivity());
+
+
+
+        //Convfigurar o recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewConversas.setLayoutManager(layoutManager);
+        recyclerViewConversas.setHasFixedSize(true);
+        recyclerViewConversas.setAdapter(adapter);
+
+
+        //Configura conversas Ref
+        String identificadoUsuario = UsuarioFirebase.getIdentificadorUsuario();
+        database = ConfiguracaoFirebase.getFirebaseDataBase();
+
+        conversasRef = database.child("conversas")
+                .child(identificadoUsuario);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarConversas();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        conversasRef.removeEventListener(childEventListenerConversas);
+    }
+
+    public void recuperarConversas(){
+
+        childEventListenerConversas = conversasRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                Conversa conversa = snapshot.getValue(Conversa.class);
+                listaConversas.add(conversa);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
     }
 
 }
